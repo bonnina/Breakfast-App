93726feb1e1bc636d1a6c504c572f04ff8d8db79
bonnina <37539513+bonnina@users.noreply.github.com>
2018-11-29 17:38:09 +0200

    Add redux-saga

--- client/src/index.js
+++ client/src/index.js
@@ -4,21 +4,31 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
-import reducer from './reducers'
-import logger from 'redux-logger'
+import reducer from './reducers';
+import { watcher } from './sagas/saga';
+import logger from 'redux-logger';
+import createSagaMiddleware from "redux-saga";
 import { createStore, applyMiddleware } from 'redux';
 import WebFont from 'webfontloader';
 
+const sagaMW = createSagaMiddleware();
+
 const store = createStore(
-  reducer, {
+  reducer, /*{
     order: [], 
     details: {
-      location: '',
-      time: ''
+      location: ''
+    },
+    api: {
+      data: {},
+      success: '',
+      error: ''
     }
-  },
-  applyMiddleware(logger)
+  }, */
+  applyMiddleware(sagaMW, logger)
 );
+
+sagaMW.run(watcher);
 
 const app = () => {
 ReactDOM.render(
