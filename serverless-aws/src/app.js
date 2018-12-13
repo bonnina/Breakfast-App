149f7d57ed5219887db75e8c1617877ef36cf30b
bonnina <37539513+bonnina@users.noreply.github.com>
2018-12-13 20:27:46 +0200

    Add headers

--- serverless-aws/src/app.js
+++ serverless-aws/src/app.js
@@ -8,6 +8,11 @@ require('./db')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(helmet())
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 app.use('/api', routes)
 
 module.exports = app
