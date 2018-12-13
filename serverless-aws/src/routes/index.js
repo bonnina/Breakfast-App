4ef011374039991b0fd28fcad9c342572686a130
bonnina <37539513+bonnina@users.noreply.github.com>
2018-12-13 15:48:53 +0200

    Create routes

---
+++ serverless-aws/src/routes/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const router = express.Router()
+const Order = require('../shema')
+
+router.get('/', async (req, res, next) => {
+  const orders = await Order.find();
+
+  res.status(200).send(orders);
+})
+
+router.post('/', async (req, res, next) => {
+  const order = await Order.create(req.body);
+
+  res.status(200).send(order);
+})
+
+module.exports = router
