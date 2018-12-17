709bb26b6c7561b3ff389cad787ad15fd4b3f13e
bonnina <37539513+bonnina@users.noreply.github.com>
2018-12-17 15:57:17 +0200

    Create API tests

---
+++ client/src/test/api.test.js
@@ -0,0 +1,39 @@
+const request = require("request");
+const chai = require("chai");
+const expect = chai.expect;
+const url = process.env.REACT_APP_BACKEND_URL;
+
+describe("Test API endpoints", function(){
+  it('Adds order to database', function(done) {
+    request.post(url, (err, res) => {
+      console.log(res);
+       expect(res.statusCode).to.equal(201);
+       expect(response).to.have.property('name').with.lengthOf(1);
+    });
+    done();
+  });
+
+  it('Fetches existing orders from database', function(done) {
+    request.get(url, (req, res) => {
+       
+      expect(res.statusCode).to.equal(200);
+      expect(res.body).to.have.lengthOf.at.least(1);
+    });
+    done();
+  });
+
+  it('Deletes all orders from database', async function(done) {
+    await request.delete(url, (req, res) => {
+      expect(res.statusCode).to.equal(200);
+    });
+    
+    await request.get(url, (req, res) => {
+      expect(res.statusCode).to.equal(200);
+      expect(res.body).to.have.lengthOf(0);
+    });
+    
+    done();
+  });
+
+});
+
